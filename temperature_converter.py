8b1a7c1b5fae4485bcf540b09e0cf9d6262a5000
seoyeon77 <nac5_230105@gclass.ice.go.kr>
2023-03-22 20:10:20 +0900

    week03 temperature_converter v0.3

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,23 +1,18 @@
 while True:
-    menu = int(input("1) fahrenheit to celsius  2) celsius to farenheit   3)exit : "))
+    menu = input("1) fahrenheit to celsius   2) celsius to fahrenheit   3) exit : ")
 
-    if menu == 1:
-         fahrenheit = float(input('Enter temperature in Fahrenheit : '))
-         celsius = (fahrenheit - 32.0) * (5.0/9.0)
-         #System.out.println(fahrenheit = " degrees fahrenheit is " + celsius = "
-         #print("화씨 %.2lf도는 섭씨 %.2lf도 입니다" % (fahrenheit, celsius))
-         #print(f"화씨 {fahrenheit:.2f}도는 섭씨 {celsius:.2f}도 입니다")
-         print("화씨 {0:.2f}도는 섭씨 {1:.2f}도 입니다".format(fahrenheit, celsius))
-    elif menu ==2:
-         #(0°C × 9/5) + 32 = 32°F
-         celsius = float(input('Enter temperature in celsius : '))
-         fahrenheit = (celsius - 32.0) * (5.0 / 9.0)
-         print(f"{celsius:.2f} degrees Celsius is {fahrenheit:.2f} degrees Farenheit.")
-    elif menu == 3:
-        break # exit(1)
-    else :
-        print("program finished!")
+    if menu == "1":
+        fahrenheit = float(input("Enter temperature in Fahrenheit : "))
+        celsius = (fahrenheit - 32.0) * (5.0/9.0)
+        print(f"{fahrenheit:.2f} degrees Fahrenheit is {celsius:.2f} degrees Celsius.")
+    elif menu == "2":
+        # (0°C × 9/5) + 32 = 32°F
+        celsius = float(input("Enter temperature in Celsius : "))
+        fahrenheit = (celsius * 9.0 / 5.0) + 32.0
+        print(f"{celsius:.2f} degrees Celsius is {fahrenheit:.2f} degrees Fahrenheit.")
+    elif menu == "3":
+        break  # exit(1)
+    else:
+        print("Please choose from the given menu.")
 
-
-
-# (0°C × 9/5) + 32 = 32°F
+print("Program finished!")
